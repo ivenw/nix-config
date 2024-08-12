c03b2a6b0a11f3e64f3ba6de4b5e209b75ef3f06
Iven Winkelmann <winkelmann.iven@gmail.com>
2024-08-12 15:13:43 +0200

    disable old nixos specifics

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -13,9 +13,6 @@
 
       darwin-apply = "darwin-rebuild switch --flake ~/nix-config";
 
-      # config
-      config-nvim = "cd ~/nixos-config/home-manager/nvim && nvim";
-
       pvim = "poetry run nvim";
       ptest = "poetry run pytest";
 
@@ -24,13 +21,13 @@
       nv = "nvim";
 
       # dev shells
-      dev-rust = "nix develop ~/nixos-config#rust -c $SHELL";
-      dev-py310 = "nix develop ~/nixos-config#python310 -c $SHELL";
-      dev-py311 = "nix develop ~/nixos-config#python311 -c $SHELL";
-      dev-py312 = "nix develop ~/nixos-config#python312 -c $SHELL";
-      dev-go = "nix develop ~/nixos-config#go -c $SHELL";
-      dev-zig = "nix develop ~/nixos-config#zig -c $SHELL";
-      dev-tf = "nix develop ~/nixos-config#tf -c $SHELL";
+      # dev-rust = "nix develop ~/nixos-config#rust -c $SHELL";
+      # dev-py310 = "nix develop ~/nixos-config#python310 -c $SHELL";
+      # dev-py311 = "nix develop ~/nixos-config#python311 -c $SHELL";
+      # dev-py312 = "nix develop ~/nixos-config#python312 -c $SHELL";
+      # dev-go = "nix develop ~/nixos-config#go -c $SHELL";
+      # dev-zig = "nix develop ~/nixos-config#zig -c $SHELL";
+      # dev-tf = "nix develop ~/nixos-config#tf -c $SHELL";
     };
     oh-my-zsh = {
       enable = true;
