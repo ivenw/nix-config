cc464b467a4223b1f6029037a8f62aa298d77d7b
Iven Winkelmann <winkelmann.iven@gmail.com>
2024-08-19 14:20:53 +0200

    delete yabai

--- modules/home-manager/yabai/default.nix
+++
@@ -1,7 +0,0 @@
-{config, ...}: {
-  xdg.configFile."yabai/yabairc" = {
-    source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/nix-config/modules/home-manager/yabai/yabairc";
-  };
-}
