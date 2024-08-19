95e0ac4deb6ab475e661cb052ab4ae09cebcc634
Iven Winkelmann <winkelmann.iven@gmail.com>
2024-08-19 14:02:25 +0200

    yabai config

--- modules/home-manager/yabai/default.nix
+++ modules/home-manager/yabai/default.nix
@@ -1,5 +1,5 @@
 {config, ...}: {
-  xdg.configFile."yabai.yabairc" = {
+  xdg.configFile."yabai/yabairc" = {
     source =
       config.lib.file.mkOutOfStoreSymlink
       "${config.home.homeDirectory}/nix-config/modules/home-manager/yabai/yabairc";
