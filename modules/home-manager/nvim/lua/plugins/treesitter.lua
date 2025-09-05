d48fea5ef46b4521193720d7e461d778c3859223
Iven Winkelmann <winkelmann.iven@gmail.com>
2025-09-05 09:49:28 +0200

    Cleanup of treesitter config

--- modules/home-manager/nvim/lua/plugins/treesitter.lua
+++ modules/home-manager/nvim/lua/plugins/treesitter.lua
@@ -1,71 +1,68 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		-- rebuild parsers when updating nvim-treesitter
 		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 		---@type TSConfig
+		---@diagnostic disable-next-line: missing-fields
 		opts = {
-				ensure_installed = {},
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-							["al"] = "@loop.outer",
-							["il"] = "@loop.inner",
-							["ai"] = "@conditional.outer",
-							["ii"] = "@conditional.inner",
-							["at"] = "@comment.outer",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = "@class.outer",
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = { ["<leader>sn"] = "@parameter.inner" },
-						swap_previous = {["<leader>sp"] = "@parameter.inner" },
+			ensure_installed = {},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["al"] = "@loop.outer",
+						["il"] = "@loop.inner",
+						["ai"] = "@conditional.outer",
+						["ii"] = "@conditional.inner",
+						["at"] = "@comment.outer",
 					},
 				},
-		}
-
-	},
-	{	"nvim-treesitter/nvim-treesitter-context" },
-	{ "windwp/nvim-ts-autotag" },
-	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		opts = {
-			enable_autocmd = false,
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = { ["<leader>sn"] = "@parameter.inner" },
+					swap_previous = { ["<leader>sp"] = "@parameter.inner" },
+				},
+			},
 		},
+	},
+}, { "nvim-treesitter/nvim-treesitter-context" }, { "windwp/nvim-ts-autotag" }, {
+	"JoosepAlviste/nvim-ts-context-commentstring",
+	opts = {
+		enable_autocmd = false,
 	},
 }
