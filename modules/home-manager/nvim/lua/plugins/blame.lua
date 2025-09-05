e099d6c6d19a213107158ef182952562ead68484
Iven Winkelmann <winkelmann.iven@gmail.com>
2025-09-05 10:04:50 +0200

    Adopt lazy style to blame config

--- modules/home-manager/nvim/lua/plugins/blame.lua
+++ modules/home-manager/nvim/lua/plugins/blame.lua
@@ -2,9 +2,7 @@ return {
     {
         "FabijanZulj/blame.nvim",
         lazy = false,
-        config = function()
-            require("blame").setup()
-        end,
+        opts = {},
         keys = {
             {
                 "<leader>b",
