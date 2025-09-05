dfb3c324c09361126b30538a85089d1561faaf4b
Iven Winkelmann <winkelmann.iven@gmail.com>
2025-09-05 10:06:36 +0200

    Update catppuccin config

---
+++ modules/home-manager/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "macchiato",
+			styles = {
+				loops = { "italic" },
+				keywords = { "italic" },
+			},
+			integrations = {
+				leap = true,
+				which_key = true,
+			},
+		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+}
