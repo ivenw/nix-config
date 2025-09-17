f40d86e46e6f7a5975ca615cc8aa983d9912a7a1
Iven Winkelmann <winkelmann.iven@gmail.com>
2025-09-17 21:18:51 +0200

    Update render markdown config

--- modules/home-manager/nvim/lua/plugins/render-markdown.lua
+++ modules/home-manager/nvim/lua/plugins/render-markdown.lua
@@ -1,13 +1,19 @@
 return {
     {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
         ---@module 'render-markdown'
         ---@type render.md.UserConfig
         opts = {
             heading = {
                 width = "block",
-            }
+            },
+            dash = {
+                width = 80,
+            },
+            link = {
+                enabled = false,
+            },
         },
-    }
+    },
 }
